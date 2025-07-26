e6986c04f0a755aafe868cd5324c20f15ef11b1b
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-07-26 21:41:16 +0600

    first commit

---
+++ types.js
@@ -0,0 +1,8 @@
+var name = "Ahmed";
+console.log(name);
+var address = "717 middle Monipur, Mirpur";
+console.log(address);
+
+var isRich = true;
+console.log(isRich);
+console.log(typeof isRich);
---
+++ variable.js
@@ -0,0 +1,4 @@
+var weight = 38;
+var price = 38;
+var year = 2025;
+console.log(year);
