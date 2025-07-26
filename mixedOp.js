0371947aef59c12256b57435caf2bd8562c91131
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-07-26 23:13:10 +0600

    another commit

---
+++ mixedOp.js
@@ -0,0 +1,7 @@
+var first = 'Shafin';
+var second = ' Ahmed';
+var fullname = first + second;
+console.log(fullname);
+
+console.log(40-'20');
+console.log('40'-20);
---
+++ shorthand.js
@@ -0,0 +1,16 @@
+var price = 20;
+// price = price + 10;
+price += 10;
+console.log(price);
+
+
+var waiver = 500;
+waiver -= 200;
+console.log(waiver);
+
+
+
+var orangePrice = 30;
+orangePrice *= 3;
+console.log(orangePrice);
+
