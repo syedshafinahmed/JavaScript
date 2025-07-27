659506bc8d2778f7aa38d3185bb7ce7a51f95f13
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-07-27 22:57:28 +0600

    add

---
+++ multiple.js
@@ -0,0 +1,16 @@
+const salary = 25000;
+const isBCS = true;
+const height = 61;
+
+// if(salary > 20000 && height > 70)  {
+//     console.log("Su-----------patro");
+// }
+// else{
+//     console.log("onno patro khuji");
+// }
+if(salary > 20000 || height > 70)  {
+    console.log("esho baba kobul");
+}
+else{
+    console.log("bhag tui mokhbul");
+}
