289da2f7a42251b27dffa163f3441ea556688577
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-07-27 22:45:43 +0600

    add

---
+++ if.js
@@ -0,0 +1,12 @@
+if(13 < 10) {
+    console.log('I have a smaller value');
+}
+
+
+var biriyaniPrice = 500;
+if(biriyaniPrice < 300) {
+    console.log('mama give me biriyani');
+}
+else{
+    console.log('mama aro 200 taka!!');
+}
