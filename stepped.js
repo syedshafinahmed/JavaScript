bf2154e9f756ee336acf1f3267900e01662c5be8
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-07-28 23:34:17 +0600

    add

--- stepped.js
+++ stepped.js
@@ -1,10 +1,10 @@
-const price = 3000;
-if (price >= 5000) {
+const price = 6000;
+if (price >= 10000) {
     const discount = price *10 /100;
     const payAmount = price - discount;
     console.log(payAmount);
 } 
-else if (price > 2500){
+else if (price > 5000){
     const discount = price * 5 / 100;
     const payAmount = price - discount;
     console.log(payAmount);
