d64320867537c4fd872081ac2c525911d6a8726a
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-07-30 13:58:37 +0600

    add

---
+++ break.js
@@ -0,0 +1,15 @@
+// for ( let i = 1; i <= 15; i++) {
+//     console.log(i);
+//     if(i >= 10){
+//         break;
+//     }
+// }
+
+let n = 54;
+while(n >= 25){
+    console.log(n);
+    if(n <= 40){
+        break;
+    }
+    n--;
+}
