5c34dc4d88ae4f50ed1c4d8460ee0e2f4f3274fa
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-07-30 10:49:18 +0600

    add

---
+++ range.js
@@ -0,0 +1,5 @@
+let num = 50;
+while(num <= 100){
+    console.log(num);
+    num += 2;
+}
---
+++ sum.js
@@ -0,0 +1,7 @@
+let num = 1;
+let sum = 0;
+while(num <= 10){
+    sum += num;
+    num += 2;
+}
+console.log("Sum is: ",sum);
