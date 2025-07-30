740efc86d3659b2f265b60fc8f9020d8acb303d8
syedshafinahmed <shafinahmed.cse@gmail.com>
2025-07-30 09:51:16 +0600

    add

---
+++ while.js
@@ -0,0 +1,12 @@
+const number = 7;
+if(number< 1){
+    console.log("choto ekta number paisi");
+}
+console.log("something is coooking");
+
+
+let num = 0;
+while(num < 5){
+    console.log('looooooooooping', num);
+    num += 1;
+}
